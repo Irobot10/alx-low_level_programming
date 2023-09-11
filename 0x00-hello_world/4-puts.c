3e992e0bbc5a8e3df621659197f6f25e7b1d2043
iRobot <ebenezer70@gmail.com>
2023-09-11 01:49:56 +0100

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print string
+ *
+ * Description: First alx C file
+ * Return: Returns an integer, 0
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
