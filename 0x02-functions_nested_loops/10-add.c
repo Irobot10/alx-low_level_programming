9cd238b092082309cc1aac64f2f9956501ebc36c
iRobot <ebenezer70@gmail.com>
2023-09-24 11:17:08 +0100

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds 2 numbers
+ * @i: First parameter
+ * @j: Second parameter
+ * Description: Prints things
+ * Return: Integer
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
