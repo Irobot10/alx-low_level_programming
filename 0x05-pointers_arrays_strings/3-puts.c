08d33fa985e9f40c90392caf353ac5c76dc5b459
iRobot <ebenezer70@gmail.com>
2023-09-24 14:12:25 +0100

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line
+ * @str: Takes string character array str as Parameter
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
