94c14fde0662f7604c4149da1c4d692ed40b01d7
iRobot <ebenezer70@gmail.com>
2023-09-24 12:45:37 +0100

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Function to multiply two integers
+ * @a: First Parameter
+ * @b: Second Parameter
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
