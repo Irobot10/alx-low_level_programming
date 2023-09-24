852b74a94f47131bd0395db61e7708a38cd4785d
iRobot <ebenezer70@gmail.com>
2023-09-24 13:48:47 +0100

    REsolved bug

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,9 +5,12 @@
 */
 void print_number(int n)
 {
-	unsigned int a, n1;
+	int a, n_original, n1;
 	a = 1;
 
+	n_original = n;
+	if (n == INT_MIN)
+		n = n + 1;
 	if (n < 0)
 	{
 		n = n * -1;
@@ -24,7 +27,11 @@ void print_number(int n)
 		}
 		for (; a >= 1; a = a / 10)
 		{
-			_putchar(((n1 / a) % 10) + '0');
+			int x = (n1 / a) % 10;
+			if (a == 1 && n_original == INT_MIN)
+				_putchar((x + 1) + '0');
+			else
+				_putchar(x + '0');
 		}
 	}
 	else
